bb74172a1c3ef6019f7af2a3d01cff97359bc295
Shahad Alaskah <shahadalaskah@hotmail.com>
2022-08-06 20:02:39 +0300

    Error handling

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -13,52 +14,72 @@ public class Main {
                 "3- To update from the todo list \n" +
                 "4- To print all the todos ");
 
-        int choice=in.nextInt();
+
         ArrayList <String> todoList=new ArrayList<>();
-
+        int  choice=0;
         while (choice != -1){
-            if(choice==1){
-                System.out.println("Enter your todo name :");
-                String todoName=in.next();
-                addTodo(todoName,todoList);
+           try {
+               choice=in.nextInt();
+                if (choice == 1) {
+                    System.out.println("Enter your todo name :");
+                    String todoName = in.next();
+                    addTodo(todoName, todoList);
 
-            } else if (choice==2) {
-                System.out.println("Enter your todo name :");
-                String todoName=in.next();
-                deleteTodo(todoName,todoList);
+                } else if (choice == 2) {
+                    if (todoList.size() != 0) {
+                    System.out.println("Enter your todo name :");
+                    String todoName = in.next();
+                    deleteTodo(todoName, todoList);
+                    }else {
+                        System.out.println("Add to todo list first");
+                    }
 
-            } else if (choice==3) {
-                System.out.println("Enter the todo index that you want to replace :");
-                int todoIndex=in.nextInt();
-                System.out.println("Enter your todo name :");
-                String todoName=in.next();
-                updateTodo(todoName,todoIndex,todoList);
+                } else if (choice == 3) {
+                    if (todoList.size() != 0) {
+                    System.out.println("Enter the todo index that you want to replace :");
+                    try {
+                        int todoIndex = in.nextInt();
+                        System.out.println("Enter your todo name :");
+                        String todoName = in.next();
+                        updateTodo(todoName, todoIndex, todoList);
+                    }catch (InputMismatchException e){
+                        System.out.println("Please enter a valid number");
+                        in.nextLine();
+                    }
+                    }else {
+                        System.out.println("Add to todo list first");
+                    }
 
-            }else {
-                printTodo(todoList);
-            }
-
+                } else if (choice == 4){
+                    printTodo(todoList);
+                }else {
+                    System.out.println("Please enter a valid number");
+                }
+           }catch (InputMismatchException e){
+               System.out.println("Please enter a valid number"+choice);
+               in.nextLine();
+           }
             System.out.println("choose another choice\n" +
                     "\n" +
                     "-------");
-            choice=in.nextInt();
         }
         System.out.println("program terminated , based on user input");
-
     }
     public static void addTodo(String todoName,ArrayList<String> todoList){
         todoList.add(todoName);
         System.out.print("Your todo saved .. ");
-
     }
     public static void deleteTodo(String todoName,ArrayList<String> todoList){
-        todoList.remove(todoName);
-        System.out.print("Your todo saved .. ");
-        System.out.println(todoList);
+            todoList.remove(todoName);
+            System.out.print("Your todo saved .. ");
     }
     public static void updateTodo(String todoName,int todoIndex,ArrayList<String> todoList){
+        try {
         todoList.set(todoIndex,todoName);
         System.out.print("Your todo saved .. ");
+        }catch (IndexOutOfBoundsException e){
+                System.out.println("The todo index does not exist");
+        }
     }
     public static void printTodo(ArrayList<String> todoList){
         System.out.println(todoList);
